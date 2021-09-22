a25400ce0bf8f3a3c549e692dbb092ecc461d20c
Rahul Singh <72799980+gabbar12345@users.noreply.github.com>
2021-09-22 11:24:10 +0530

    Update check_if_added.php

--- check_if_added.php
+++ check_if_added.php
@@ -2,7 +2,7 @@
 
 function check_if_added($items_id){
     $user_id=$_SESSION['user_id'];
-    include 'common.php';
+    $con=mysqli_connect("localhost","root","","store");
     $elect="SELECT * FROM user_items WHERE items_id='$items_id' AND user_id='$user_id'";
     $query=mysqli_query($con,$elect);
     
